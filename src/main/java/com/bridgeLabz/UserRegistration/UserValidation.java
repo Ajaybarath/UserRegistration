555509a1b2397ed977201bf6f4b14228ae84651d
ajaybarath <ajaybarath3@gmail.com>
2021-09-12 19:09:38 +0530

    ADDED: method to check if the first name is valid

--- src/main/java/com/bridgeLabz/UserRegistration/UserValidation.java
+++ src/main/java/com/bridgeLabz/UserRegistration/UserValidation.java
@@ -1,8 +1,19 @@
 package com.bridgeLabz.UserRegistration;
 
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 public class UserValidation {
 
 	public static void main(String args[]) {
 		System.out.println("Hello world");
+		
+		Scanner s = new Scanner(System.in);
+		
+		String firstName = s.next();
+		
+		boolean checkFirstName = Pattern.matches("[A-Z][a-zA-Z]{3,}", firstName);
+		
+		System.out.println("First name check : " + checkFirstName);
 	}
 }
