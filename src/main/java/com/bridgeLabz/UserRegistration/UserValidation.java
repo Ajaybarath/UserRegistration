0f81869fade8d0b57f875a62f74ac08b52bb2654
ajaybarath <ajaybarath3@gmail.com>
2021-09-12 19:11:30 +0530

    ADDED: method to check if the second name is valid

--- src/main/java/com/bridgeLabz/UserRegistration/UserValidation.java
+++ src/main/java/com/bridgeLabz/UserRegistration/UserValidation.java
@@ -7,13 +7,20 @@ public class UserValidation {
 
 	public static void main(String args[]) {
 		System.out.println("Hello world");
-		
+
 		Scanner s = new Scanner(System.in);
-		
+
 		String firstName = s.next();
-		
+
 		boolean checkFirstName = Pattern.matches("[A-Z][a-zA-Z]{3,}", firstName);
-		
+
 		System.out.println("First name check : " + checkFirstName);
+
+		String secondName = s.next();
+
+		boolean checkSecondName = Pattern.matches("[A-Z][a-zA-Z]{3,}", secondName);
+
+		System.out.println("Second name check : " + checkSecondName);
+
 	}
 }
