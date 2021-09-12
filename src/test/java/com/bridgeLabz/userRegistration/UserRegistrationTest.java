adc5e74a3d2017f6e11dcd1b180c65db743363de
ajaybarath <ajaybarath3@gmail.com>
2021-09-12 20:52:31 +0530

    ADDED: function to test firstName validation

--- src/test/java/com/bridgeLabz/userRegistration/UserRegistrationTest.java
+++ src/test/java/com/bridgeLabz/userRegistration/UserRegistrationTest.java
@@ -2,17 +2,22 @@ package com.bridgeLabz.userRegistration;
 
 import org.junit.Test;
 
+import com.bridgeLabz.UserRegistration.UserValidation;
+
+import junit.framework.Assert;
+
 public class UserRegistrationTest {
 	
 	@Test
 	public void testFirstAndLastName() {
 		
 		String firstName = "Ajay";
-		String lastName = "Barath";
 		
+		UserValidation userValidation = new UserValidation();
 		
+		boolean checkFirstName = userValidation.checkFirstName(firstName);
 		
-		
+		Assert.assertEquals(true, checkFirstName);
 	}
 
 }
