49befb0e935693f137efd678a88850cdce249a61
ajaybarath <ajaybarath3@gmail.com>
2021-09-21 12:33:05 +0530

    REFRACTORED: renamed the file to UserValidation

--- src/main/java/com/bridgeLabz/UserRegistration/UserValidation.java
+++ src/main/java/com/bridgeLabz/UserRegistration/UserValidation.java
@@ -3,103 +3,53 @@ package com.bridgeLabz.UserRegistration;
 import java.util.Scanner;
 import java.util.regex.Pattern;
 
-import com.bridgeLabz.UserRegistration.InvalidUserDetailException.ExceptionType;
+interface Validation{
+	boolean validate(String regex, String input);
+	
+	public static void printResult(String regex, String input, String parameter, Validation validation) {
+		
+		if (validation.validate(regex, input) == true) {
+			System.out.println("Valid " + parameter);
+		}
+		else {
+			System.out.println("Invalid " + parameter);
+		}
+	}
+}
 
 public class UserValidation {
-
-	public static void main(String args[]) throws InvalidUserDetailException {
-		System.out.println("Hello world");
-
+	
+	public static String inputCall(String parameter) {
 		Scanner s = new Scanner(System.in);
-
-		UserValidation userValidation = new UserValidation();
-
-		String firstName = s.nextLine();
-
-		boolean checkFirstName = userValidation.checkFirstName(firstName);
-		System.out.println("First name check : " + checkFirstName);
-
-		String secondName = s.nextLine();
-
-		boolean checkSecondName = userValidation.checkSecondName(secondName);
-
-		System.out.println("Second name check : " + checkSecondName);
-
-		String email = s.nextLine();
-
-		boolean checkEmail = userValidation.checkEmail(email);
-
-		System.out.println("Email check : " + checkEmail);
-
-		String phone = s.nextLine();
-
-		boolean checkPhone = userValidation.checkPhone(phone);
-
-		System.out.println("Phone number check : " + checkPhone);
-
-		String password = s.nextLine();
-
-		boolean checkPassword = userValidation.checkPassword(password);
-
+		System.out.println("Enter " + parameter);
+		return s.nextLine();
 	}
+	
+	public static void main(String args[]) {
 
-	public boolean checkFirstName(String detail) throws InvalidUserDetailException {
-		boolean checkFirstName = Pattern.matches("[A-Z][a-zA-Z]{3,}", detail);
-
-		if (checkFirstName != true) {
-			throw new InvalidUserDetailException(ExceptionType.INVALID_FIRST_NAME, "Enter a valid first name");
-		}
-
-		return true;
-	}
-
-	public boolean checkSecondName(String detail) throws InvalidUserDetailException {
-		boolean check = Pattern.matches("[A-Z][a-zA-Z]{3,}", detail);
-
-		if (check != true) {
-			throw new InvalidUserDetailException(ExceptionType.INVALID_FIRST_NAME, "Enter a valid second name");
-		}
-
-		return true;
-	}
-
-	public boolean checkEmail(String detail) throws InvalidUserDetailException {
-		boolean check = Pattern.matches("([a][b][c][.]?){1}([a-zA-Z]{3,})?[@]([b][l][.][c][o][.]?[i]?[n]?)", detail);
-
-		if (check != true) {
-			throw new InvalidUserDetailException(ExceptionType.INVALID_FIRST_NAME, "Enter a valid email");
-		}
-
-		return true;
-	}
-
-	public boolean checkPhone(String detail) throws InvalidUserDetailException {
-		boolean check = Pattern.matches("([9][1][ ])[0-9]{10}", detail);
-
-		if (check != true) {
-			throw new InvalidUserDetailException(ExceptionType.INVALID_FIRST_NAME, "Enter a valid phone number");
-		}
-
-		return true;
-	}
-
-	public boolean checkPassword(String password) throws InvalidUserDetailException {
-		boolean rule1 = Pattern.matches(".{8,}", password);
-
-		boolean rule2 = Pattern.matches("(?=.*[A-Z]).+", password);
-
-		boolean rule3 = Pattern.matches("(?=.*\\d).+", password);
-
-		boolean rule4 = Pattern.matches("(?=.*[.,:;\\'!@#$%^&*_+=|(){}]).+", password);
+		Validation userDetailCheck = (String regex, String input) -> {
+			return Pattern.matches(regex, input);
+		};
 		
-		boolean check = rule1 && rule2 && rule3 && rule4;
 
-		if (check != true) {
-			throw new InvalidUserDetailException(ExceptionType.INVALID_FIRST_NAME, "Enter a valid password");
-		}
+		String firstName = inputCall("FirstName");
 
-		return true;
+		String secondName = inputCall("SecondName");
 
+		String email = inputCall("Email");
+
+		String phone = inputCall("Phone");
+
+		String password = inputCall("Password");
+		
+		Validation.printResult("[A-Z][a-zA-Z]{3,}",firstName, "FirstName", userDetailCheck);
+		Validation.printResult("[A-Z][a-zA-Z]{3,}",secondName, "SecondName", userDetailCheck);
+		Validation.printResult("([a][b][c][.]?){1}([a-zA-Z]{3,})?[@]([b][l][.][c][o][.]?[i]?[n]?)",email, "Email", userDetailCheck);
+		Validation.printResult("([9][1][ ])[0-9]{10}",phone, "Phone", userDetailCheck);
+		Validation.printResult("^(.{8,})*((?=.*[A-Z]).+)*((?=.*\\\\d).+)*((?=.*[.,:;\\\\'!@#$%^&*_+=|(){}]).+)$",password, "Password", userDetailCheck);
+
+
+		
 	}
 
 }
